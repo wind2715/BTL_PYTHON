4ad4178f9e0e59d3aea8104d911b58754c942af4
Tuan Vu <tuanvu2102003@gmail.com>
2023-10-18 11:31:20 +0700

    Trua 18/10

--- test.py
+++ test.py
@@ -1,14 +1,28 @@
-from tkinter import *
-from PIL import Image, ImageTk
+import tkinter as tk
+from tkinter import ttk
 
-root = Tk()
+def get_previous_value():
+    current_value = combobox.get()
+    current_index = combobox.current()
 
-# Tạo hình ảnh từ một tệp ảnh (ví dụ: icon.png)
-image = Image.open("icon\previous.png")
-photo = ImageTk.PhotoImage(image)
+    if current_index > 0:
+        previous_index = current_index - 1
+        previous_value = combobox['values'][previous_index]
+        print(f"Giá trị đứng trên {current_value} là {previous_value}")
+    else:
+        print(f"{current_value} là giá trị đầu tiên, không có giá trị đứng trên")
 
-# Tạo label và đặt hình ảnh vào label
-label = Label(root, image=photo)
-label.pack()
+root = tk.Tk()
+root.title("Lấy giá trị đứng trên Combobox")
+
+# Tạo combobox
+combobox = ttk.Combobox(root)
+combobox['values'] = ["Mục 1", "Mục 2", "Mục 3"]
+
+combobox.pack()
+
+# Tạo button để lấy giá trị đứng trên
+button = tk.Button(root, text="Lấy giá trị đứng trên", command=get_previous_value)
+button.pack()
 
 root.mainloop()
